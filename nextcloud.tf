5024cc71e435141723c545067207e950c985c14a
Erik Stambaugh <erik@erikstambaugh.com>
2021-12-26 13:26:29 -0800

    Initial terraform configuration

--- nextcloud.tf
+++ nextcloud.tf
@@ -1,14 +1,13 @@
 
+provider "aws" {
+  region  = "us-west-2" # XXX make this configurable
+}
+
 resource "random_pet" "name" {}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "nextcloud-${random_pet.name.id}"
   acl    = "private"
-
-//  tags = {
-//    Name        = "My bucket"
-//    Environment = "Dev"
-//  }
 }
 
 
